2cfdb2a3a84aaad88723c3033cdfeaf09871f964
abid72rizvi <52654896+rizvi0708abid@users.noreply.github.com>
2019-07-10 12:36:54 +0530

    Remove Friends
    After getting her PhD, Christie has become a celebrity at her university, and her facebook profile is full of friend requests. Being the nice girl she is, Christie has accepted all the requests.
    
    Now Kuldeep is jealous of all the attention she is getting from other guys, so he asks her to delete some of the guys from her friend list.
    
    To avoid a 'scene', Christie decides to remove some friends from her friend list, since she knows the popularity of each of the friend she has, she uses the following algorithm to delete a friend.
    
    Algorithm Delete(Friend):
        DeleteFriend=false
        for i = 1 to Friend.length-1
             if (Friend[i].popularity < Friend[i+1].popularity)
                delete i th friend
                DeleteFriend=true
                break
        if(DeleteFriend == false)
            delete the last friend
    
    Input: 
    First line contains T number of test cases. First line of each test case contains N, the number of friends Christie currently has and K ,the number of friends Christie decides to delete. Next lines contains popularity of her friends separated by space.
    
    Output: 
    For each test case print N-K numbers which represent popularity of Christie friend's after deleting K friends.
    
    Constraints
    1<=T<=1000
    1<=N<=100000
    0<=K< N 
    0<=popularity_of_friend<=100
    
    NOTE:
    Order of friends after deleting exactly K friends should be maintained as given in input.
    
    SAMPLE INPUT 
    3
    3 1
    3 100 1
    5 2
    19 12 3 4 17
    5 3
    23 45 11 77 18
    SAMPLE OUTPUT 
    100 1 
    19 12 17 
    77 18

---
+++ RemoveFriends.java
@@ -0,0 +1,163 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+
+class Node {
+	
+	String val ;
+	Node next ;
+	
+	public Node(String val) {
+		super();
+		this.val = val;
+		this.next = null ;
+	}
+	
+	
+}
+
+//LinkList Class having the utility functions for implementing the link list.
+class LinkList {
+	
+	private Node head ;
+	
+	//Method for adding Node to a link list
+	public void add(String val){
+		
+		Node n = new Node(val);
+		
+		if(head==null){
+			head = n;
+		}
+		else{
+			Node curr = head ;
+			
+			while(curr.next!=null){
+				curr = curr.next ;
+			}
+			curr.next = n ;
+		}
+	}
+	
+	
+	//Method for printing the link list elements
+	public StringBuilder print(Node head){
+		StringBuilder out = new StringBuilder();
+		if(head==null){
+			//System.out.print("Empty List!");
+			return null ;
+		}
+		else{
+			Node curr = head ;
+			
+				while(curr!=null){
+				   // System.out.print(curr.val + " ");
+				    out.append(curr.val + " ");
+			    	curr = curr.next ;
+				}
+			}
+			return out ;
+		}
+	
+	//Method to reverse a link list provided with the head node using recursion
+	public Node reverse(Node n) {
+		
+		if(n==null || n.next==null){
+			return n ;
+		}
+		Node reverseHead = reverse(n.next);
+		n.next.next = n ;
+		n.next = null ;
+		return reverseHead ;
+	}
+
+    
+    //setter for head node
+	public void setHead(Node head) {
+		this.head = head;
+	}
+
+    //Getter for head node
+	public Node getHead() {
+		return this.head;
+	}
+
+    //method to get the last element of the link list.
+	public Node getLastNode(Node head) {
+		if(head==null){
+			return head ;
+		}
+		
+		while(head.next!=null){
+			head = head.next;
+		}
+		return head ;
+	}
+	
+	
+	public void removeFromLast(){
+		Node curr = head ;
+		if(curr==null || curr.next == null) head = null;
+		else{
+			while(curr.next.next!=null){
+				curr=curr.next ;
+			}
+			curr.next = null ;
+		}
+		
+	}
+	
+}
+
+//Runner class 
+class TestClass {
+    
+    
+    public static void main(String [] args) throws Exception {
+		
+	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int  T = Integer.parseInt(br.readLine());
+        StringBuilder output = new StringBuilder("");
+        while(T-->0){
+            String [] inps = br.readLine().split(" ");
+            int K = Integer.parseInt(inps[1]);
+            String [] values = br.readLine().split(" ");
+            
+            LinkList list = new LinkList();
+            for(String s : values){
+                list.add(s);
+            }
+           
+           //code to remove the nodes whose values are less than next node
+           Node prev = null ;
+           Node curr = list.getHead();
+           
+           while(curr.next!=null){
+               
+               if(K >0 && (Integer.parseInt(curr.val) < Integer.parseInt(curr.next.val))){
+                   if(prev==null) list.setHead(curr.next);
+                   else{
+                      prev.next = curr.next ;
+                   }
+                    prev = null ;
+                    curr = list.getHead();
+                    K-- ;
+               }else{
+                   prev = curr;
+                   curr = curr.next ;
+               }
+           }
+           
+           if(K>0){
+               while(K-->0) list.removeFromLast();
+           }
+           StringBuilder sb = list.print(list.getHead());
+           sb.append("\n");
+           output.append(sb);
+        }
+          System.out.print(output);
+    }
+    
+}
